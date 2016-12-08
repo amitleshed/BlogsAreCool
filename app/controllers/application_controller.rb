a5f1c3b14e45f6e7be05511e54e478b84f1624be
amitleshed <amitleshed@amits-MacBook-Pro.local>
2016-12-08 13:26:25 +0200

    Fix devise sign-in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :remember_me, :image) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :current_password, :image) }
+ devise_parameter_sanitizer.for(:sign_up) << :image
+ devise_parameter_sanitizer.for(:sign_up) << :username
+ devise_parameter_sanitizer.for(:account_update) << :image
+ devise_parameter_sanitizer.for(:account_update) << :username
   end
 end
