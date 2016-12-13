80e7d969ab1b0b3b99be17a27ce110e50349969d
amitleshed <amitleshed@amits-mbp.mynet>
2016-12-13 21:41:09 +0200

    Generate controller conversations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap-sprockets
 //= require material
 //= require jquery_ujs
+//= require chat
 //= require turbolinks
 //= require_tree .
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
   has_many :votes
   has_many :comments
+  has_many :conversations, :foreign_key => :sender_id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
