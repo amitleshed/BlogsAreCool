beed9528920ac0a68d3e016235b16878ef511f05
amitleshed <amitleshed@amits-MacBook-Pro.local>
2016-12-15 13:54:55 +0200

    Show articles from welcome page, Repick colors.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all.order('username DESC')
+    @articles = Article.all
+    @comment = Comment.new
 
     if params[:search]
       @users = User.search(params[:search])
