e577600022f333a44611f5e57c97cee13304784d
amitleshed <amitleshed@icloud.com>
2016-12-19 12:43:44 +0200

    Crop images automatically

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
-Paperclip::Attachment.default_options[:url] = 'blogsarecool.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_region] = 'us-east-1'
+if Rails.env.production? 
+  Paperclip::Attachment.default_options[:url] = 'blogsarecool.s3.amazonaws.com'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:s3_region] = 'us-east-1'
+end
