8654fe3e3bbfd24c69c2d239dc0df878db738385
amitleshed <amitleshed@icloud.com>
2016-12-20 10:34:03 +0200

    Search field for unsigned users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { medium: "300x300#", small: "100x100#" }, default_url: "missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300#", small: "100x100#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def self.search(parameter) 
