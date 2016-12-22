08d76032b95d569c92b92e24246362e513440f8a
amitleshed <amitleshed@icloud.com>
2016-12-22 15:05:12 +0200

    Deploying

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
 module UsersHelper
-      def redner_with_hashtags_user(about)
-    about.gsub(/#\w+/){|word| link_to word, "/profiles/hashtag/#{word.delete('#')}", :id => "hashtags-user"}.html_safe
-  end
+    def redner_with_hashtags_user(about)
+      if about != nil
+      about.gsub(/#\w+/){|word| link_to word, "/profiles/hashtag/#{word.delete('#')}", :id => "hashtags-user"}.html_safe
+    end
+    end
 end
