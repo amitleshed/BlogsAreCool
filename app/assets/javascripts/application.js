d1e66e4d2faeedd3516e4d8d32805c863935690b
amitleshed <amitleshed@icloud.com>
2016-12-29 17:20:06 +0200

    Fix menu on-click bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //=jquery.scrollfire.min.js
-//= require jquery_ujs
 //= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
