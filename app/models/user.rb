b3c6d9eb56eab6264f3b4c99c9b18b3365f6f9b3
amitleshed <amitleshed@icloud.com>
2017-01-02 12:14:44 +0200

    Minor modifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300#", small: "100x100#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :username, uniqueness: true
+  validates :email, uniqueness: true
 
   after_create do
    user = User.find_by(id: self.id)
